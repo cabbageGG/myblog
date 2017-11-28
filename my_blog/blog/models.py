a2a1bbfd128d5a24a5d56ef6492b701b1d3160e1
yangjin <13246856469@163.com>
2017-11-28 23:29:43 +0800

    add index view

--- my_blog/blog/models.py
+++ my_blog/blog/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+class Blog(models.Model):
+    titile = models.CharField(max_length=32, default="Title")
+    content = models.TextField(null=True)
+    create_time = models.DateTimeField(null=True)
+
+
--- my_blog/blog/views.py
+++ my_blog/blog/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
 from django.views.generic.base import View
+from blog import models
+from datetime import datetime
 # Create your views here.
 
 class IndexView(View):
     def get(self, request):
-        return render(request, "index.html")
+        # time = datetime.now()
+        # models.Blog.objects.create(titile="my second blog",content="good good study, day day up !",create_time=time)
+        blogs = models.Blog.objects.all()
+        return render(request, "index.html", {"blogs":blogs})
