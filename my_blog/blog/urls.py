234fcff0ef34100964ca861bd8b74fcf3729ed50
yangjin <13246856469@163.com>
2017-12-04 17:04:26 +0800

    remove edit and save blog module, admin has same feature

--- my_blog/blog/urls.py
+++ my_blog/blog/urls.py
@@ -8,9 +8,9 @@ from . import views
 urlpatterns = [
     url(r'^$',views.blogs, name="index"),
     url(r'^page/(?P<blog_id>\d+)/$', views.show_blog, name="show_blog"),
-    url(r'^edit/(?P<blog_id>\d+)/$', views.edit_blog, name="edit_blog"),
-    url(r'^save/$', views.save_blog, name="save_blog"),
-    url(r'^delete/(?P<blog_id>\d+)/$', views.delete_blog, name="delete_blog"),
+    # url(r'^edit/(?P<blog_id>\d+)/$', views.edit_blog, name="edit_blog"),
+    # url(r'^save/$', views.save_blog, name="save_blog"),
+    # url(r'^delete/(?P<blog_id>\d+)/$', views.delete_blog, name="delete_blog"),
     url(r'^signin/$', views.signin, name="signin"),
     url(r'^register/$', views.register, name="register"),
     url(r'^signin_action/$', views.signin_action, name="signin_action"),
--- my_blog/blog/views.py
+++ my_blog/blog/views.py
@@ -18,33 +18,33 @@ def show_blog(request, blog_id):
     blog = models.Blog.objects.get(id=blog_id)
     return render(request, "blog/blog.html", {"blog": blog})
 
-def edit_blog(request, blog_id):
-    if str(blog_id) == '0':
-        return render(request, "blog/blog_edit.html")
-    blog = models.Blog.objects.get(id=blog_id)
-    return render(request, "blog/blog_edit.html", {"blog": blog})
-
-def save_blog(request):
-    title = request.POST.get("title", "TITLE")
-    summary = request.POST.get("summary", "")
-    content = request.POST.get("content", "")
-    blog_id = request.POST.get("blog_id", "0")
-    if blog_id == '0':
-        models.Blog.objects.create(title=title, summary=summary, content=content, create_time=datetime.now())
-        blogs = models.Blog.objects.all()
-        return render(request, "blog/blogs.html", {"blogs": blogs})
-    blog = models.Blog.objects.get(id=blog_id)
-    blog.title = title
-    blog.summary = summary
-    blog.content = content
-    blog.create_time = datetime.now()
-    blog.save()
-    return render(request, "blog/blog.html", {"blog": blog})
-
-def delete_blog(request, blog_id):
-    models.Blog.objects.filter(id=blog_id).delete()
-    blogs = models.Blog.objects.all()
-    return render(request, "blog/blogs.html", {"blogs": blogs})
+# def edit_blog(request, blog_id):
+#     if str(blog_id) == '0':
+#         return render(request, "blog/blog_edit.html")
+#     blog = models.Blog.objects.get(id=blog_id)
+#     return render(request, "blog/blog_edit.html", {"blog": blog})
+#
+# def save_blog(request):
+#     title = request.POST.get("title", "TITLE")
+#     summary = request.POST.get("summary", "")
+#     content = request.POST.get("content", "")
+#     blog_id = request.POST.get("blog_id", "0")
+#     if blog_id == '0':
+#         models.Blog.objects.create(title=title, summary=summary, content=content, create_time=datetime.now())
+#         blogs = models.Blog.objects.all()
+#         return render(request, "blog/blogs.html", {"blogs": blogs})
+#     blog = models.Blog.objects.get(id=blog_id)
+#     blog.title = title
+#     blog.summary = summary
+#     blog.content = content
+#     blog.create_time = datetime.now()
+#     blog.save()
+#     return render(request, "blog/blog.html", {"blog": blog})
+#
+# def delete_blog(request, blog_id):
+#     models.Blog.objects.filter(id=blog_id).delete()
+#     blogs = models.Blog.objects.all()
+#     return render(request, "blog/blogs.html", {"blogs": blogs})
 
 def comment_blog(request, blog_id):
     pass
