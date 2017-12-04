03c9b65d4e2e81c7ee52ea2811e2616cce3cbd94
yangjin <13246856469@163.com>
2017-12-04 16:52:18 +0800

    modify

--- my_blog/blog/admin.py
+++ my_blog/blog/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from blog.models import Blog, User
 
 # Register your models here.
+class BlogAdmin(admin.ModelAdmin):
+    list_display = ('title', 'create_time')
+    search_fields = ('title', 'create_time')
+
+admin.site.register(Blog, BlogAdmin)
+
+admin.site.register(User)
--- my_blog/blog/models.py
+++ my_blog/blog/models.py
@@ -7,12 +7,17 @@ class Blog(models.Model):
     content = models.TextField(null=True)
     summary = models.TextField(null=True)
     create_time = models.DateTimeField(null=True)
+    def __unicode__(self):
+        return self.title
 
 class User(models.Model):
     name = models.CharField(max_length=32)
     head_img = models.ImageField(upload_to='head_img', null=True)
     account = models.CharField(max_length=32, null=True)
     passwd = models.CharField(max_length=32, null=True)
+    def __unicode__(self):
+        return self.name
+
 
 
 
