9cf8149aa1705a01d8a7b2549f1c8933f3434556
yangjin <13246856469@163.com>
2017-12-12 19:52:42 +0800

    use cookies to stay login

--- my_blog/blog/views.py
+++ my_blog/blog/views.py
@@ -1,9 +1,8 @@
 # -*- coding: utf-8 -*-
 from django.shortcuts import render
-from django.views.generic.base import View
 from blog import models
 from datetime import datetime
-from django.http import HttpResponse, HttpResponseRedirect
+from django.http import HttpResponseRedirect
 # Create your views here.
 
 def index(request):
@@ -32,43 +31,17 @@ def show_blog(request, blog_id):
     comments = models.Comments.objects.filter(blog_id=blog_id, comment_id=0)
     return render(request, "blog/blog.html", {"blog": blog, "comments":comments})
 
-# def edit_blog(request, blog_id):
-#     if str(blog_id) == '0':
-#         return render(request, "blog/blog_edit.html")
-#     blog = models.Blog.objects.get(id=blog_id)
-#     return render(request, "blog/blog_edit.html", {"blog": blog})
-#
-# def save_blog(request):
-#     title = request.POST.get("title", "TITLE")
-#     summary = request.POST.get("summary", "")
-#     content = request.POST.get("content", "")
-#     blog_id = request.POST.get("blog_id", "0")
-#     if blog_id == '0':
-#         models.Blog.objects.create(title=title, summary=summary, content=content, create_time=datetime.now())
-#         blogs = models.Blog.objects.all()
-#         return render(request, "blog/blogs.html", {"blogs": blogs})
-#     blog = models.Blog.objects.get(id=blog_id)
-#     blog.title = title
-#     blog.summary = summary
-#     blog.content = content
-#     blog.create_time = datetime.now()
-#     blog.save()
-#     return render(request, "blog/blog.html", {"blog": blog})
-#
-# def delete_blog(request, blog_id):
-#     models.Blog.objects.filter(id=blog_id).delete()
-#     blogs = models.Blog.objects.all()
-#     return render(request, "blog/blogs.html", {"blogs": blogs})
-
 def comment_blog(request, blog_id):
     user_name = request.POST.get("user_name", "游客")
+    account = request.COOKIES.get("account","")
+    if account:
+        user = models.User.objects.filter(account=account)
+        user_name = user[0].name
     blog_id = request.POST.get("blog_id", "1")
     comment_id = request.POST.get("comment_id", "0")
     content = request.POST.get("content", "")
     models.Comments.objects.create(username=user_name, blog_id=blog_id, comment_id=comment_id, content=content, create_time=datetime.now())
-    blog = models.Blog.objects.get(id=blog_id)
-    comments = models.Comments.objects.filter(blog_id=blog_id, comment_id=0)
-    return render(request, "blog/blog.html", {"blog": blog, "comments":comments})
+    return HttpResponseRedirect("/blog/page/%s" % blog_id)
 
 def signin(request):
     return render(request, "blog/signin.html")
@@ -81,26 +54,27 @@ def signin_action(request):
     password = request.POST.get("password", "")
     user = models.User.objects.filter(account=account,passwd=password)
     if user:
-        return HttpResponseRedirect('/blog')
-    return render(request, "blog/signin.html")
+        response = HttpResponseRedirect("/blog")
+        response.set_cookie(key='account', value=account, expires=3600)
+        return response
+    return render(request, "blog/signin.html", {"login_fail":True})
 
 def register_action(request):
     name = request.POST.get("name", "")
     account = request.POST.get("account", "")
     password1 = request.POST.get("password1", "")
     password2 = request.POST.get("password2", "")
-    if password1 != password2 or password1 == "" or password2 == "":
-        #return render(request, "blog/register.html")
-        return HttpResponse("密码错误")
+    if  password1 == "" or password2 == "":
+        return render(request, "blog/register.html",{"msg":"密码不能为空"})
+    if password1 != password2:
+        return render(request, "blog/register.html", {"msg": "两次密码不相同"})
     if account:
         user = models.User.objects.filter(account=account)
         if user:
-            #return render(request, "blog/register.html")
-            return HttpResponse("用户已存在")
+            return render(request, "blog/register.html", {"msg":"用户已存在"})
         else:
             models.User.objects.create(name=name, account=account, passwd=password1)
             return HttpResponseRedirect('/blog')
     else:
-        #return render(request, "blog/register.html")
-        return HttpResponse("账户为空")
+        return render(request, "blog/register.html", {"msg":"账户为空"})
 
