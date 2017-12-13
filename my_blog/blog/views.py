ffdae820df11ecdac7507cb60ed6b2d4bae89c07
yangjin <13246856469@163.com>
2017-12-13 20:05:36 +0800

    register donot use ajax

--- my_blog/blog/views.py
+++ my_blog/blog/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render
 from blog import models
 from datetime import datetime
 from django.http import HttpResponseRedirect
+from django.views.decorators.csrf import csrf_exempt
 # Create your views here.
 
 def index(request):
