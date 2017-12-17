c383c25aa5784db7479365fa4f3563174d40bcb0
yangjin <13246856469@163.com>
2017-12-17 13:17:10 +0800

    修改过滤器，让blog中正常显示html标签的尖括号。

--- my_blog/blog/templatetags/myfilter.py
+++ my_blog/blog/templatetags/myfilter.py
@@ -8,6 +8,8 @@ register = Library()
 
 @register.filter
 def replaceTags(content):
+    content = content.replace("<", "&lt;")  # 预处理换行
+    content = content.replace(">", "&gt;")  # 预处理换行
     content = content.replace(" ", "&nbsp;")  # 预处理空格
     content = content.replace("\n", "<br/>")  # 预处理换行
     return content
