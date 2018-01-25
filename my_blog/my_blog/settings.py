f89e24ef6415d8aa2b84e8f3a9a74123fbd9bbb6
yangjin <13246856469@163.com>
2018-01-25 16:57:33 +0800

    beauty the admin

--- my_blog/my_blog/settings.py
+++ my_blog/my_blog/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = ['*', 'www.liyangjin.club', '127.0.0.1']
 # Application definition
 
 INSTALLED_APPS = [
+    'blog.apps.SuitConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
